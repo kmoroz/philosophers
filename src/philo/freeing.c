7771f6a60526a890f64e3e6435eec65a7537542a
kmoroz <ksmorozova@hotmail.com>
2021-10-07 18:14:33 +0100

    rewrite the fn freeing forks

--- src/philo/freeing.c
+++ src/philo/freeing.c
@@ -6,20 +6,19 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/06 15:25:16 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/10/06 15:31:57 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/10/07 18:02:54 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "philo.h"
 
-void	free_forks(t_settings *settings)
+void	free_fork(int size, pthread_mutex_t *fork)
 {
-	pthread_mutex_t	*fork;
-	int				i;
+	int	i;
 
-	fork = settings->philo[0].table->fork;
-	while (i < settings->philo_size)
+	i = 0;
+	while (i < size)
 	{
 		pthread_mutex_destroy(&fork[i]);
 		i++;
@@ -29,6 +28,6 @@ void	free_forks(t_settings *settings)
 
 void	free_everything(t_settings *settings)
 {
-	free_forks(settings);
+	free_fork(settings->philo_size, settings->philo->table->fork);
 	free(settings->philo);
 }
